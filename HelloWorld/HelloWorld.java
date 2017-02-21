88fbb9839363d5a1ff7f79df5499010821033e88
Eric Liu <erliu@rubiconproject.com>
2017-02-20 20:57:57 -0500

    create hello world

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * My first Java file. My motivation for learning Java is that it is the world's
+ * most popular language and I'd like to experience what everyone else is
+ * experiencing to relate to them.
+ *
+ * Also, I come from a Javascript background, so this will be easy, since well,
+ * Java is just Javascript minus the "-script"... Kidding.
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
